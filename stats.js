cdc62ab8b993d896142024622affd68ed1b8b7d2
bialasky <bialas96@gmail.com>
2018-06-19 11:45:24 +0200

    (task 3 working)

--- stats.js
+++ stats.js
@@ -30,11 +30,47 @@
 	    { date: new Date('2018-01-29'), visits: 65 },
 	    { date: new Date('2018-01-30'), visits: 42 },
 	    { date: new Date('2018-01-31'), visits: 48 },
+		]
+
+		let series2 = [
+		 { date: new Date('2018-01-01'), visits: 361 },
+	    { date: new Date('2018-01-02'), visits: 43 },
+	    { date: new Date('2018-01-03'), visits: 51 },
+	    { date: new Date('2018-01-04'), visits: 56 },
+	    { date: new Date('2018-01-05'), visits: 13 },
+	    { date: new Date('2018-01-06'), visits: 64 },
+	    { date: new Date('2018-01-07'), visits: 13 },
+		 { date: new Date('2018-01-08'), visits: 32 },
+	    { date: new Date('2018-01-09'), visits: 54 },
+	    { date: new Date('2018-01-10'), visits: 43 },
+	    { date: new Date('2018-01-11'), visits: 76 },
+	    { date: new Date('2018-01-12'), visits: 13 },
+	    { date: new Date('2018-01-13'), visits: 716 },
+	    { date: new Date('2018-01-14'), visits: 95 },
+		 { date: new Date('2018-01-15'), visits: 37 },
+	    { date: new Date('2018-01-16'), visits: 64 },
+	    { date: new Date('2018-01-17'), visits: 31 },
+	    { date: new Date('2018-01-18'), visits: 36 },
+	    { date: new Date('2018-01-19'), visits: 38 },
+	    { date: new Date('2018-01-20'), visits: 31 },
+	    { date: new Date('2018-01-21'), visits: 56 },
+		 { date: new Date('2018-01-22'), visits: 34 },
+	    { date: new Date('2018-01-23'), visits: 123 },
+	    { date: new Date('2018-01-24'), visits: 236 },
+	    { date: new Date('2018-01-25'), visits: 75 },
+	    { date: new Date('2018-01-26'), visits: 32 },
+	    { date: new Date('2018-01-27'), visits: 23 },
+	    { date: new Date('2018-01-28'), visits: 75 },
+	    { date: new Date('2018-01-29'), visits: 23 },
+	    { date: new Date('2018-01-30'), visits: 85 },
+	    { date: new Date('2018-01-31'), visits: 23 },
 		] 
 
+		let totalSeries = series1.concat(series2);
+
 const getAverage = (series1, boolean) => {
-		let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 		let visits = 0;
+
 		let monday = 0;
 		let tuesday = 0;
 		let wednesday = 0;
@@ -50,6 +86,8 @@ const getAverage = (series1, boolean) => {
 		let fridays = 0;
 		let saturdays = 0;
 		let sundays = 0;
+
+		
 
 		const averageVisits = (day, howmany) => {
 			day = day / howmany;
@@ -96,7 +134,13 @@ const getAverage = (series1, boolean) => {
 					default:
 					break;
 				}
-			}	return(monday + " " + mondays + " Monday avarage visits:" + averageVisits(monday, mondays) );
+			}	return("Monday avarage visits:" + averageVisits(monday, mondays) + " \n" +
+								 "Tuesday avarage visits:" + averageVisits(tuesday tuesdays) + " \n" +
+								 "Wednesday avarage visits:" + averageVisits(wednesday, wednesdays) + " \n" +
+								 "Thursday avarage visits:" + averageVisits(thursday, thursdays) + " \n" +
+								 "Friday avarage visits:" + averageVisits(friday, fridays) + " \n" +
+								 "Saturday avarage visits:" + averageVisits(saturday, saturdays) + " \n" +
+								 "Sunday avarage visits:" + averageVisits(sunday, sundays + " \n" +);
 		}
 
 };
